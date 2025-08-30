a2204e1e8469075b7724dc7c2748907b8c2b8e89
skvsr <135157405+skvsr@users.noreply.github.com>
2025-08-29 23:29:22 -0300

    git commit

--- src/jogo.c
+++ src/jogo.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+int main(){
+
+printf("-------------------------------------------------");
+printf("             BEM VINDO AO RPG GAME               ");
+printf("-------------------------------------------------");
+
+
+
+}
