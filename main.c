3010bcddcd146b55476b2a88c24c6455c519a2e9
skvsr <135157405+skvsr@users.noreply.github.com>
2025-09-01 20:58:18 -0300

    git commit .

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+int main(){
+
+printf("-------------------------------------------------\n");
+printf("             BEM VINDO AO RPG GAME               \n");
+printf("-------------------------------------------------\n");
+
+printf("                    JOGO POR                     \n");
+printf("        Samuel Pinheiro e Samuel Rodrigues       \n");
+
+
+
+}
